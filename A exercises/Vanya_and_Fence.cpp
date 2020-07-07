4ac37706e6ed75b279b2fc71d9c00f8fe856cd27
ReyVega <a01114523@itesm.mx>
2020-07-06 23:01:52 -0500

    Competitive-Programming

--- A exercises/Vanya_and_Fence.cpp
+++ A exercises/Vanya_and_Fence.cpp
@@ -5,22 +5,15 @@ using namespace std;
 int main() {
     int n = 0,
         h = 0,
-        *friends,
+        tmp = 0,
         result = 0;
 
     cin >> n >> h;
 
-    friends = new int[n];
-
     for(int i = 0; i < n; i++) {
-        cin >> friends[i];
-        if(friends[i] > h) {
-            result += 2;
-        } else {
-            result++;
-        }
+        cin >> tmp;
+        result = (tmp > h) ? result + 2 : result + 1;
     }
     cout << result;
-    delete[] friends;
     return 0;
 }
