456d432723be569124c164fb6d28e5a047336fb0
ReyVega <a01114523@itesm.mx>
2020-08-01 14:08:10 -0500

    Competitive Programming

--- ICPC-CF-A/CF443-D2-A.cpp
+++ ICPC-CF-A/CF443-D2-A.cpp
@@ -12,6 +12,7 @@ int main() {
     set<char> s;
 
     getline(cin,input);
+    
     for(int i = 0; i < input.length(); i++) {
         // isalpha checks if a character is alphabetic
         if(isalpha(input[i])) {
