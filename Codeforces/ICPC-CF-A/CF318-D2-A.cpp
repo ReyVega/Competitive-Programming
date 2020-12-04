f30bf9148e5470535267ec6d6f6d8230475c1c50
ReyVega <a01114523@itesm.mx>
2020-12-04 01:16:47 -0600

    Competitive Programming

---
+++ Codeforces/ICPC-CF-A/CF318-D2-A.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+
+    int n = 0,
+        k = 0;
+    
+    cin >> n;
+    cin >> k;
+
+    if(k % 2 != 0) {
+        cout << ;
+    } else {
+
+    }
+    return 0;
+}
